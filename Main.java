ff89302cea522ce4922fcb31f44833ca57f31aa5
gustavoassunc <gustavoassunc.andrade@gmail.com>
2024-07-31 12:50:11 -0300

    atv 31/07

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+ 
+        Academia conta1=null;
+
+        conta1 = new Academia("Davi", "Rua das peras 1000", "41 99109-4358");
        
-        System.out.println("Faça sua atividade");
- 
+        System.out.println(conta1.toString());
+         conta1.setNome("davi 2");
     }
+ }
  
-}
