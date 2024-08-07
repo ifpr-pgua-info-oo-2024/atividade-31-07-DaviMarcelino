18d2f804c62eda9dd41ed8a8a4f5c5eb9bb22435
Leandro Clayton Pivovarsky <leandroclaytonp@gmail.com>
2024-08-07 11:04:04 -0300

    a

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
  
         Academia conta1=null;
 
-        conta1 = new Academia("Davi", "Rua das peras 1000", "41 99109-4358");
+        conta1 = new Academia("Davi","Rua das peras 1000", "41 99109-4358");
        
         System.out.println(conta1.toString());
          conta1.setNome("davi 2");
