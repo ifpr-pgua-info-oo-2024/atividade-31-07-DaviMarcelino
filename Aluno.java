79fedf4c57e3c3041c5cf7a9d5bc93d68a2e63b3
Leandro Clayton Pivovarsky <leandroclaytonp@gmail.com>
2024-08-07 10:48:04 -0300

    a

---
+++ Aluno.java
@@ -0,0 +1,74 @@
+import java.sql.Date;
+
+public class Aluno {
+
+    private String nome;
+    private String graduacao;
+    private String genero;
+    private Date nascimento;
+    private Double altura;
+    private Double peso;
+
+    public Aluno (String nome, String graduacao, Date nascimento, String genero, int i, int j) {
+
+        this.nome = nome;
+        this.graduacao = graduacao;
+        this.nascimento = nascimento;
+        this.genero = genero;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public Aluno (String nome, String graduacao, Date nascimento) {
+        this(nome, graduacao, nascimento);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getGraduacao() {
+        return graduacao;
+    }
+
+    public void setGraduacao(String graduacao) {
+        this.graduacao = graduacao;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento (Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+}
